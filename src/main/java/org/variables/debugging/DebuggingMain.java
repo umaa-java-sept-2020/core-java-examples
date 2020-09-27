a5d315cc934277812c685251d098bd2d96eef765
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2020-09-27 09:21:29 +0530

    debugging code example

--- src/main/java/org/variables/debugging/DebuggingMain.java
+++ src/main/java/org/variables/debugging/DebuggingMain.java
@@ -7,16 +7,25 @@ public class DebuggingMain {
 
     public static void main(String[] args) {
         DebuggingMain debuggingMain = new DebuggingMain();
+        System.out.println("debug1");
+        System.out.println("debug2");
+        System.out.println("debug3");
+        System.out.println("debug4");
         debuggingMain.sum(10, 20);
+        System.out.println("debug5");
     }
 
     public DebuggingMain()
     {
         System.out.println("inside constructor");
     }
+
     public void sum(int x, int y)
     {
         int result = x + y;
+        System.out.println("some more lines");
+        System.out.println("some more lines");
+        System.out.println("some more lines");
         System.out.println(result);
     }
 }
