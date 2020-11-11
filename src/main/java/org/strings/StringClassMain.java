18dfb8b347a8b1c0cad7b34fc56a7d52a80b1388
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2020-11-11 09:06:15 +0530

    string class methods | session 2

--- src/main/java/org/strings/StringClassMain.java
+++ src/main/java/org/strings/StringClassMain.java
@@ -16,9 +16,131 @@ public class StringClassMain {
         //  m8();
         // m9();
 
-        m10();
+        //  m10();
+
+        //   m11();
+
+        //  m12();
+
+//        m13();
+
+        //    m14();
+
+        //  m15();
+        //  m16();
+
+        //   m17();
+
+       // m18();
+     //   m19();
+
+      //  m20();
+
+        m21();
     }
 
+    public static void m21()
+    {
+        String s = " Welcome ";
+
+        for(int i = 0; i <s.length(); i++ )
+        {
+            System.out.println(s.charAt(i));
+        }
+    }
+    public static void m20()
+    {
+        String s = " Welcome to india ";
+        char ch[] = s.toCharArray();
+        System.out.println(Arrays.toString(ch));
+    }
+    // lowerCase, upperCase, trim
+    public static void m19() {
+        String s = " Welcome to india "; // string are constant, immutable
+        String result = s.toLowerCase();
+        System.out.println(result);
+        System.out.println(s);
+
+        result = s.toUpperCase();
+        System.out.println(result);
+        System.out.println(s);
+
+        System.out.println(s.length());
+        result = s.trim();
+        System.out.println(result.length());
+    }
+
+    // startsWith, endsWith, substring, subsequence
+    public static void m18() {
+        String s = "welcome to india";
+        boolean flag = s.startsWith("welcome");
+        boolean flag1 = s.endsWith("india");
+        System.out.println(flag);
+        System.out.println(flag1);
+        CharSequence result = s.subSequence(1, 4);
+        System.out.println(result);
+        String s1 = s.substring(1, 4);
+        System.out.println(s1);
+
+        s1 = s.substring(4); // from the 4 th array index it will start collecting
+        System.out.println(s1);
+    }
+
+    public static void m17() {
+        String s = "welcome to india";
+        String[] array = s.split(" ");
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void m16() {
+        String s = "welcome to india";
+        s = s.replaceFirst("welcome", "hi");
+        System.out.println(s);
+    }
+
+    public static void m15() {
+        String s = "welcome to india";
+        s = s.replace('i', 'l');
+        System.out.println(s);
+    }
+
+    // KMP algorithm : string comparison
+    public static void m14() {
+        String s = "we are from india";
+        boolean flag = s.matches(s);
+        System.out.println(flag);
+    }
+
+    public static void m13() {
+        String s = "we are from india";
+        int x = s.length();
+        System.out.println(x);
+    }
+
+    public static void m12() {
+        String s1 = null;
+        String s = new String(s1); //new String();  "";
+        System.out.println(s.isEmpty());
+    }
+
+    public static void m11() {
+        String s1 = new String("abc"); // object reference from heap (scp + heap)
+        String s2 = "abc"; // scp
+
+        System.out.println(s1.hashCode() + " " + System.identityHashCode(s1));
+        System.out.println(s2.hashCode() + " " + System.identityHashCode(s2));
+
+        System.out.println(System.identityHashCode(s1)); // heap hashcode
+        System.out.println(System.identityHashCode(s1.intern())); // scp hashcode
+
+        String s3 = s1; // assigning heap object refernce to s3
+        String s4 = s1.intern(); // assigning scp object reference to s4
+
+        System.out.println(s3.hashCode());
+        System.out.println(s4.hashCode());
+    }
+
+    // indexOf
     public static void m10() {
         String s = "abc abc";
         byte[] bytes = s.getBytes();
@@ -29,7 +151,8 @@ public class StringClassMain {
         System.out.println(index);
         index = s.lastIndexOf('b');
         System.out.println(index);
-
+        index = s.indexOf("ab", 2);
+        System.out.println(index);
     }
 
     public static void m9() {
