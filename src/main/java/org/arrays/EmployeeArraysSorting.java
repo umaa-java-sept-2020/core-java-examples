bdea57a6573efaa29c440563c03e4ec2b0e59540
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2020-12-02 08:33:01 +0530

    comparator with compare(o1,o2) method

--- src/main/java/org/arrays/EmployeeArraysSorting.java
+++ src/main/java/org/arrays/EmployeeArraysSorting.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 
 public class EmployeeArraysSorting {
 
-    public static void main(String[] args) {
+    public static void m2(String[] args) {
         Employee e1 = new Employee(1, "John", "FICO");
         Employee e2 = new Employee(2, "Doe", "TCS");
         Employee e3 = new Employee(3, "Alex", "FICO");
@@ -20,31 +20,61 @@ public class EmployeeArraysSorting {
     }
 
 
-    public static void m1(String[] args) {
+    public static void main(String[] args) {
 
         Employee e1 = new Employee(1, "John", "FICO");
         Employee e2 = new Employee(2, "Doe", "TCS");
         Employee e3 = new Employee(3, "Alex", "FICO");
         Employee e4 = new Employee(4, "Carrey", "ACCENTURE");
+        Employee e5 = new Employee(5, "John", "GOOGLE");
 
         // sort employee array by company name
 
-        Employee[] employees = {e1, e2, e3, e4};
+        Employee[] employees = {e1, e2, e3, e4, e5};
 
-        CompanyNameComparator companyNameComparator = new CompanyNameComparator();
-        Arrays.sort(employees, companyNameComparator);
+        Comparator companyNameComparator = new CompanyNameComparator();
+        Comparator nameComparator = new NameComparator();
+        Comparator nameReverseCompanyNameComparator = new NameReverseCompanyNameComparator();
+
+        Arrays.sort(employees, nameReverseCompanyNameComparator);
         System.out.println(Arrays.toString(employees));
 
     }
 
     // Comparator: to sort the objects: logic
-    private static class CompanyNameComparator implements Comparator<Employee>
-    {
+    private static class CompanyNameComparator implements Comparator<Employee> {
         @Override
         public int compare(Employee o1, Employee o2) {
             String c1 = o1.getCompanyName();
             String c2 = o2.getCompanyName();
             return c1.compareTo(c2);
+        }
+    }
+
+    private static class NameComparator implements Comparator<Employee> {
+        @Override
+        public int compare(Employee o1, Employee o2) {
+            String c1 = o1.getName();
+            String c2 = o2.getName();
+            int result = -c1.compareTo(c2); // reverseOrder -
+            return result;
+        }
+    }
+
+    // if name is same then sort in reverse order of company name
+    private static class NameReverseCompanyNameComparator implements Comparator<Employee> {
+        @Override
+        public int compare(Employee o1, Employee o2) {
+            String c1 = o1.getName();
+            String c2 = o2.getName();
+            int result = -c1.compareTo(c2);
+            if (result == 0) {
+                String companyName1 = o1.getCompanyName();
+                String companyName2 = o2.getCompanyName();
+
+                result = -companyName1.compareTo(companyName2);
+            }
+            return result;
         }
     }
 }
