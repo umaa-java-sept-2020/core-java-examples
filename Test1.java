1754338adb87e735ad10adbde7902649581340c0
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2021-01-30 20:45:39 +0530

    exception | error | catch the error

---
+++ Test1.java
@@ -0,0 +1,16 @@
+
+
+public class Test1
+{
+
+    public static void main(String[] args)
+    {
+        Handler handler = new Handler();
+        System.out.println("hi");
+    }
+
+    private static class Handler
+    {
+
+    }
+}
